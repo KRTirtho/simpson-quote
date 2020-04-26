bb0bd5ace351955af3fcb157e7a4e7bdff6e8a8a
KR Tirtho <kbroy2016@gmail.com>
2020-04-26 18:09:11 +0600

    Updates

---
+++ precache-manifest.7c9e8a57ff7cc51fac040d8605e6ce4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22db3cf79d9e51ad040cd219f75273e5",
+    "url": "/project_simpson_quote_FCC/index.html"
+  },
+  {
+    "revision": "5d0d33f9e85721f6c602",
+    "url": "/project_simpson_quote_FCC/static/css/main.ad3eab41.chunk.css"
+  },
+  {
+    "revision": "f92eccc7272be28816bb",
+    "url": "/project_simpson_quote_FCC/static/js/2.6f63a7ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project_simpson_quote_FCC/static/js/2.6f63a7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0d33f9e85721f6c602",
+    "url": "/project_simpson_quote_FCC/static/js/main.de9e7c55.chunk.js"
+  },
+  {
+    "revision": "7c27fd0c66ea9f65ba6b",
+    "url": "/project_simpson_quote_FCC/static/js/runtime-main.a9106489.js"
+  }
+]);
