90e712a4f9402c4e6fe04852edfa39245e2123d0
KRTirtho <krtirtho@gmail.com>
2021-06-04 16:59:21 +0600

    fixed initial issues

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ const urlImg = 'https://thesimpsonsquoteapi.glitch.me/quotes'
 
 // const fetchQuote = fetch()
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props)
     this.state = {
       data: [],
@@ -15,61 +15,71 @@ class App extends React.Component{
       updateData: null,
       imgData: [],
       imgPos: [],
-      loaded: false
     }
     this.handleClick = this.handleClick.bind(this)
-  }  
-
-  componentDidMount(){
-     fetch(url)
-     .then(res=> res.json())
-     .then(json =>{
-       this.setState({
-         loaded : true,
-         data: json.quotes
-       })
-     }).catch(err=> console.error(err +' from '+url+err));
-     fetch(urlImg)
-     .then(res=> res.json())
-     .then(json =>{
-       this.setState({
-         loadedImg : true,
-         imgData: json[0].image,
-         imgPos: json[0].characterDirection
-       })
-     }).catch(err=> console.error('Failed to load image from'+urlImg+err))
   }
-  handleClick(){
+
+  componentDidMount() {
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          loaded: true,
+          data: json.quotes
+        })
+      }).catch(err => console.error(err + ' from ' + url + err));
+    fetch(urlImg)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          loadedImg: true,
+          imgData: json[0].image,
+          imgPos: json[0].characterDirection
+        })
+      }).catch(err => console.error('Failed to load image from' + urlImg + err))
+  }
+  handleClick() {
     const randomNumb = Math.floor(Math.random() * this.state.data.length)
     const newQuote = this.state.data[randomNumb]
     fetch(urlImg)
-     .then(res=> res.json())
-     .then(json =>{
-       this.setState({
-         loadedImg : true,
-         imgData: json[0].image,
-         updateData: newQuote,
-         imgPos: json[0].characterDirection
-       })
-     }).catch(err=> console.error('Failed to load image from'+urlImg+err))
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          loadedImg: true,
+          imgData: json[0].image,
+          updateData: newQuote,
+          imgPos: json[0].characterDirection
+        })
+      }).catch(err => console.error('Failed to load image from' + urlImg + err))
   }
 
-  render(){
-    var {loaded, data, loadedImg} = this.state
-    if(!loaded && !loadedImg){
+  render() {
+    var { loaded, loadedImg } = this.state
+    if (!loaded && !loadedImg) {
       return (
         <div id="loading">Loading</div>
       )
     }
-    else{
+    else {
       return (
         <div className="bg" >
           <div id="quote-box">
-              {this.state.imgPos === 'Right'?<img style={{float:'right', maxWidth: 200, maxHeight: 200}} src={this.state.imgData}/>:<img style={{float:'left', maxWidth: 200, maxHeight: 200}} src={this.state.imgData}/>}
-              {this.state.updateData===null?<p id="text">{this.state.data[0].quote}</p>:<p id="text">{this.state.updateData.quote}</p>}
-              {this.state.updateData===null?<p id="author">-{this.state.data[0].author}</p>:<p id="author">-{this.state.updateData.author}</p>}
-              <button className="twitter"><a id="tweet-quote" target="_blank" href="https://twitter.com/intent/tweet"><i className="fab fa-twitter"></i></a></button>
-              <button id="new-quote" onClick={this.handleClick}>FeedMeMore</button>
+            {this.state.imgPos === 'Right' ?
+              <img
+                style={{ float: 'right', maxWidth: 200, maxHeight: 200 }} src={this.state.imgData} alt="Simpson"
+              /> :
+              <img style={{ float: 'left', maxWidth: 200, maxHeight: 200 }} src={this.state.imgData} alt="Simpson" />
+            }
+            {this.state.updateData === null ?
+              <p id="text">{this.state.data[0].quote}</p> :
+              <p id="text">{this.state.updateData.quote}</p>
+            }
+            {this.state.updateData === null ?
+              <p id="author">-{this.state.data[0].author}</p> :
+              <p id="author">-{this.state.updateData.author}</p>
+            }
+            <button className="twitter"><a id="tweet-quote" target="_blank" rel="noopener noreferrer" href="https://twitter.com/intent/tweet"><i className="fab fa-twitter"></i></a></button>
+            <button id="new-quote" onClick={this.handleClick}>FeedMeMore</button>
           </div>
         </div>
       )
